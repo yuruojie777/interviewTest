5a75564534d72693d8108a245a382c676a2699a8
Yu Ruojie <yuruojie777@163.com>
2022-09-18 00:58:02 +1000

    add logo and navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 // import logo from './logo.svg';
+import { Divider } from '@mui/material';
 import './App.css';
 import { Contact } from './Contacts';
 
 function App() {
   return (
-    <>
-        <h1>Build by Ruojie</h1>
+    <div>
+      <div className='navigation'>
+        <div className='logo'>
+        </div>
+        <div>
+          <nav>
+            <ul>
+              <li><a href='blog'>Blog</a></li>
+              <li><a href='tech'>Tech</a></li>
+              <li><a href='movie'>Movie</a></li>
+              <li><a href='about'>About</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <Divider/>
+      <div  className='wrapper'>
         <Contact/>
-    </>
+      </div>
+    </div>
   );
 }
 
